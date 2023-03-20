8dd61bec921b9ff5a985f320f6b3f64f6c329985
Rahul Gaur <rahul.gaur@altudo.co>
2023-03-20 16:14:19 +0530

    useMemo hook

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import UseEffect2Hook from './components/useEffect2Hook';
 import UseEffecthook from './components/useEffectHook';
+import UseMemoHook from './components/useMemohook';
 import UseStateHook from './components/useStateHook';
 
 function App() {
@@ -12,6 +13,8 @@ function App() {
       <UseEffect2Hook />
       <h1>UseEffect hook Example 2</h1>
       <UseEffecthook />
+      <h1>Usememo hook example</h1>
+      <UseMemoHook />
     </>
   );
 }
---
+++ react-hooks/src/components/useMemohook.js
@@ -0,0 +1,29 @@
+import { useState, useMemo, useEffect } from "react";
+const UseMemoHook = () => {
+    const [number, setNumber] = useState(0);
+    const [dark, setDark] = useState(false);
+    const doubleNumber = useMemo(() => {
+        return slowfunction(number);
+    }, [number]);
+    const themeStyle = useMemo(() => {
+        return {
+            backgroundColor: dark ? 'black' : 'white',
+            color: dark ? 'white' : 'black'
+        }
+    }, [dark])
+    useEffect(() => {
+        console.log('theme chnaged')
+    }, [themeStyle])
+    function slowfunction(num) {
+        console.log('calling slow function');
+        for (let i = 0; i < 1000000; i++) { }
+        return num * 2;
+    }
+    return (<>
+        <input type='number' value={number} onChange={e => setNumber(parseInt(e.target.value))} />
+        <button onClick={() => { setDark(prevDark => !prevDark) }}>Change Theme</button>
+        <div style={themeStyle}>{doubleNumber}</div>
+    </>)
+
+}
+export default UseMemoHook;
